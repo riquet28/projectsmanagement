c8b4058f2e6b9ec5d41c1d441316d001ceda1e04
Eric <eric.merlin3@gamil.com>
2020-10-06 02:38:51 +0200

    Ajout de la gem fontawesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,3 +21,4 @@ document.addEventListener("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip()
   $('[data-toggle="popover"]').popover()
 })
+import "@fortawesome/fontawesome-free/js/all";
